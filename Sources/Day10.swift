1d87b3284a24c545255d3b5926cf31d22addee89
helcioItiyama <lco.itiyama@gmail.com>
2025-02-19 20:17:29 -0300

    AOC Day 11

---
+++ Sources/Day10.swift
@@ -0,0 +1,87 @@
+import Algorithms
+
+// This code was based on HyperNeutrino's
+
+struct Day10: AdventDay {
+  var data: String
+
+  var entities: [[Int]] {
+    data
+      .split(separator: "\n")
+      .map { $0.split(separator: "").compactMap { Int($0) }}
+  }
+  
+  func part1() -> Int {
+    var trailheads = [(Int, Int)]()
+    var sum = 0
+    
+    for r in 0..<entities.count {
+      for c in 0..<entities[0].count {
+        if entities[r][c] == 0 { trailheads.append((r, c)) }
+      }
+    }
+    
+    func score(_ grid: [[Int]], _ r: Int, _ c: Int) -> Int {
+      var queue = [(r, c)]
+      var summits = Set<Position>()
+      while !queue.isEmpty {
+        let (cr, cc) = queue.removeFirst()
+        for (nr, nc) in [(cr, cc + 1), (cr, cc - 1), (cr + 1, cc), (cr - 1, cc)] {
+          if nr < 0 || nc < 0 || nr >= grid.count || nc >= grid[0].count { continue }
+          if grid[nr][nc] != grid[cr][cc] + 1 { continue }
+          if grid[nr][nc] == 9 {
+            summits.insert(Position(x: nr, y: nc))
+          } else {
+            queue.append((nr, nc))
+          }
+        }
+      }
+      return summits.count
+    }
+    
+    for (r, c) in trailheads {
+      sum += score(entities, r, c)
+    }
+    
+    return sum
+  }
+
+  func part2() -> Int {
+    var trailheads = [(Int, Int)]()
+    var sum = 0
+    
+    for r in 0..<entities.count {
+      for c in 0..<entities[0].count {
+        if entities[r][c] == 0 { trailheads.append((r, c)) }
+      }
+    }
+    
+    func score(_ grid: [[Int]], _ r: Int, _ c: Int) -> Int {
+      var queue = [(r, c)]
+      var traits = 0
+      while !queue.isEmpty {
+        let (cr, cc) = queue.removeFirst()
+        for (nr, nc) in [(cr, cc + 1), (cr, cc - 1), (cr + 1, cc), (cr - 1, cc)] {
+          if nr < 0 || nc < 0 || nr >= grid.count || nc >= grid[0].count { continue }
+          if grid[nr][nc] != grid[cr][cc] + 1 { continue }
+          if grid[nr][nc] == 9 {
+            traits += 1
+          } else {
+            queue.append((nr, nc))
+          }
+        }
+      }
+      return traits
+    }
+    
+    for (r, c) in trailheads {
+      sum += score(entities, r, c)
+    }
+    
+    return sum
+  }
+}
+
+
+
+
---
+++ Sources/Day11.swift
@@ -0,0 +1,68 @@
+import Algorithms
+
+struct Day11: AdventDay {
+  var data: String
+
+  var entities: [Int] {
+    data
+      .split(whereSeparator: { $0.isWhitespace })
+      .compactMap { Int($0) }
+  }
+  
+  func part1() -> Int {
+    func operation(_ stone: Int, _ i: Int) -> Int {
+      if i == 0 { return 1 }
+      var results = 0
+      
+      if stone == 0 {
+        results += operation(1, i - 1)
+      } else if String(stone).count % 2 == 0 {
+        let stringStone = String(stone)
+        let half = stringStone.count / 2
+        let leading = Int(stringStone.prefix(half)) ?? 0
+        let trailing = Int(stringStone.suffix(half)) ?? 0
+        results += operation(leading, i - 1) + operation(trailing, i - 1)
+      } else {
+        results = operation(stone * 2024, i - 1)
+      }
+      
+      return results
+    }
+    
+    return entities.reduce(0) { acc, curr in acc + operation(curr, 25) }
+  }
+
+  func part2() -> Int {
+    var cache = [Position: Int]()
+    
+    func operation(_ stone: Int, _ i: Int) -> Int {
+      if i == 0 { return 1 }
+      
+      if !cache.keys.contains(Position(x: stone, y: i)) {
+        var results = 0
+        
+        if stone == 0 {
+          results = operation(1, i - 1)
+        } else if String(stone).count % 2 == 0 {
+          let stringStone = String(stone)
+          let half = stringStone.count / 2
+          let leading = Int(stringStone.prefix(half)) ?? 0
+          let trailing = Int(stringStone.suffix(half)) ?? 0
+          results += operation(leading, i - 1) + operation(trailing, i - 1)
+        } else {
+          results = operation(stone * 2024, i - 1)
+        }
+        
+        cache[Position(x: stone, y: i)] = results
+      }
+      
+      return cache[Position(x: stone, y: i), default: 0]
+    }
+    
+    return entities.reduce(0) { acc, curr in acc + operation(curr, 75) }
+  }
+}
+
+
+
+
